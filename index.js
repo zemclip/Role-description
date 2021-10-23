54cf208316d71932fe435ef1d79d90aa0813f115
zemclip <39643032+zemclip@users.noreply.github.com>
2021-10-23 16:14:03 +0900

    Update index.js

--- index.js
+++ index.js
@@ -3,8 +3,33 @@ const changeCategory = (element) => {
     const grand = parent.parentElement;
     grand.getElementsByClassName('is-active')[0].classList.remove('is-active');
     parent.classList.add('is-active');
+    changeCategoryVisible(element.id);
 }
 
-const changeVisible = (id) => {
-    
+const changeCategoryVisible = (id) => {
+    const list = document.getElementById('list');
+    const all = Array.from(list.children).filter((element) => {
+        return element.tagName === 'A';
+    });
+    const target = Array.from(list.getElementsByClassName(id));
+
+    if (id === 'all') {
+        all.map((element) => {
+            element.style.display = '';
+        });
+        return;
+    }
+
+    all.map((element) => {
+        element.style.display = 'none';
+    });
+
+    target.map((element) => {
+        element.style.display = '';
+    });
+}
+
+const changeCardVisible = (element) => {
+    const target = element.getElementsByTagName('div')[0];
+    target.style.display = target.style.display === '' ? 'none' : '';
 }
